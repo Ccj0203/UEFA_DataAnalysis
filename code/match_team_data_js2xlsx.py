c59c33e4a3efe6393b92cf384951a1f4418b96fd
Ccj0203 <417982164@qq.com>
2024-10-24 19:15:00 +0800

    球队数据分析长表及宽表整理

--- code/match_team_data_js2xlsx.py
+++ code/match_team_data_js2xlsx.py
@@ -1,9 +1,10 @@
 import json
 import pandas as pd
+import os
 
-if __name__ == '__main__':
-    with open('..\\data\\match_team_data.json','r',encoding='utf-8') as f:
-        js_match_team_data = json.load(f)
+def json_to_long(json_text):
+
+    js_match_team_data = json_text
 
     output = []
     for match in js_match_team_data:
@@ -25,6 +26,20 @@ if __name__ == '__main__':
                     stat_trans = ''
                 output.append([match_id, team_id, stat_name, stat_value, stat_unit, stat_trans])
     
-    pd.DataFrame(output, columns=['match_id', 'team_id', 'stat_name', 'stat_value', 'stat_unit', 'stat_trans']).to_excel('..\\data\\match_team_data.xlsx',index=False)
-        
+    return pd.DataFrame(output, columns=['match_id', 'team_id', 'stat_name', 'stat_value', 'stat_unit', 'stat_trans'])
+    
+
+if __name__ == '__main__':
+    print(os.getcwd())
+    with open('data\\match_team_data.json','r',encoding='utf-8') as f:
+        js_match_team_data = json.load(f)
+
+    match_team_data_long = json_to_long(js_match_team_data)
+    match_team_data_long.to_excel('data\\match_team_data_LONG.xlsx',index=False)
+    # match_team_data_long.to_excel('..\\data\\match_team_data_LONG.xlsx',index=False)
+
+    match_team_data_wide = match_team_data_long.pivot(index=['match_id', 'team_id'], columns='stat_name', values='stat_value')
+    match_team_data_wide.to_excel('data\\match_team_data_WIDE.xlsx',index=True)
+    # match_team_data_wide.to_excel('..\\data\\match_team_data_WIDE.xlsx',index=True)
+
 
