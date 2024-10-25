062e05824f4c8411c765e4b5273adc74de5fba59
Ccj0203 <417982164@qq.com>
2024-10-25 14:50:43 +0800

    整理了指标

--- code/match_team_data_js2xlsx.py
+++ code/match_team_data_js2xlsx.py
@@ -30,16 +30,18 @@ def json_to_long(json_text):
     
 
 if __name__ == '__main__':
-    print(os.getcwd())
+    
+    # .py的工作路径是整个project的根目录，读取数据文件直接进入data目录
     with open('data\\match_team_data.json','r',encoding='utf-8') as f:
         js_match_team_data = json.load(f)
 
     match_team_data_long = json_to_long(js_match_team_data)
     match_team_data_long.to_excel('data\\match_team_data_LONG.xlsx',index=False)
-    # match_team_data_long.to_excel('..\\data\\match_team_data_LONG.xlsx',index=False)
+    # 整理一份stat_name的表，后续可用AI工具翻译出中文
+    match_team_data_long[['stat_name']].drop_duplicates().reset_index(drop=True).to_excel('data\\match_team_data_STAT_NAME.xlsx',index=False)
 
     match_team_data_wide = match_team_data_long.pivot(index=['match_id', 'team_id'], columns='stat_name', values='stat_value')
     match_team_data_wide.to_excel('data\\match_team_data_WIDE.xlsx',index=True)
-    # match_team_data_wide.to_excel('..\\data\\match_team_data_WIDE.xlsx',index=True)
+
 
 
